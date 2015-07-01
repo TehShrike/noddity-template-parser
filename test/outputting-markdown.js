05ce762fa2d7d2ac8c53f8efc51c01585f898abc
Josh Duff <me@JoshDuff.com>
2015-07-01 13:15:22 -0500

    Changing another test to use markdown: false

--- test/outputting-markdown.js
+++ test/outputting-markdown.js
@@ -7,13 +7,13 @@ test('Embeds a template and spits out markdown', function(t) {
 	var state = testState()
 
 	state.retrieval.addPost('file1.md', { title: 'Some title', date: new Date() }, 'Check it out ::file2.md:: \n\n[^1]:	*Collected Sermons* volume 10, p. 310.\n')
-	state.retrieval.addPost('file2.md', { title: 'Some title', date: new Date() }, 'lol yeah')
+	state.retrieval.addPost('file2.md', { title: 'Some title', date: new Date(), markdown: false }, 'lol yeah')
 
 	var renderer = new Renderer(state.butler, linkify)
 
 	state.butler.getPost('file1.md', function(err, post) {
 		renderer.renderMarkdown(post, function(err, html) {
-			t.equal(html, 'Check it out <p>lol yeah</p>\n \n\n[^1]:	*Collected Sermons* volume 10, p. 310.\n')
+			t.equal(html, 'Check it out lol yeah \n\n[^1]:	*Collected Sermons* volume 10, p. 310.\n')
 
 			state.butler.stop()
 			t.end()
