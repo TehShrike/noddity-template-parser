086cc672f715a954c94da3ea0340a1e57607b1f8
Joseph Dykstra <josephdykstra@gmail.com>
2015-09-26 20:41:25 -0500

    Make a bit of progress, maybe

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var htmlify = require('./htmlify.js')
-var turnEmbeddedTemplatesIntoHtmlElements = require('./noddity-template-transformer.js')
+var turnContentIntoAnArrayOfObjects = require('./noddity-template-transformer.js')
 
 module.exports = function (post, linkifier, options) {
 	var html = options && options.convertToHtml === false ? post.content : htmlify(post)
 	html = linkifier.linkify(html)
-	html = turnEmbeddedTemplatesIntoHtmlElements(html)
-	return html
+	return turnContentIntoAnArrayOfObjects(html)
 }
--- noddity-template-transformer.js
+++ noddity-template-transformer.js
@@ -1,20 +1,40 @@
-var stringTemplate = require('string-template/compile')
 var getTemplateDataObject = require('./parse-template-arguments.js')
-var uuid = require('random-uuid-v4')
-
-var generatePostSpan = stringTemplate('<span class=\'noddity-template\' data-noddity-post-file-name=\'{0}\' data-noddity-template-arguments=\'{1}\' data-noddity-partial-name=\'{2}\'>{3}</span>')
+var execAll = require('regexp.execall')
 
 function turnNoddityTemplatesIntoHtmlElements(html) {
+	var result = []
+	var finished = false
+	var startSearchIndex = 0
+
+	execAll(/<code>.+?<\/code>|::.+?::/, html).forEach(function (matches))
+		var searchString = html.slice(startSearchIndex)
+		var found = searchString.search()
+		if (found === -1) {
+			finished = true
+		} else if (searchString[found] === ':') {
+			if (found > startSearchIndex) {
+				result.push({
+					type: 'string',
+					value: searchString.slice(0, found - 1) // not sure about the "- 1" bit
+				})
+			}
+			var templateText = searchString.exec(/::([^:]+?)::/)
+			var pieces = templateText.split('|')
+			var filename = pieces.shift()
+			var args = getTemplateDataObject(pieces)
+			result.push({
+				type:'template',
+				filename: filaname,
+				arguments: args
+			})
+		} else {
+			searchString
+		}
+	}
 	return html.replace(/<code[\s\S]+?<\/code|::(.+?)::/gm, function(match, templateText) {
 		if (match.slice(0, 5) === '<code') {
 			return match
 		} else {
-			var partialName = uuid()
-			var pieces = templateText.split('|')
-			var postName = pieces.shift()
-			var templateObject = getTemplateDataObject(pieces)
-			var templateArguments = JSON.stringify(templateObject)
-			return generatePostSpan(postName, templateArguments, partialName, '{{>' + partialName + '}}')
 		}
 	})
 }
