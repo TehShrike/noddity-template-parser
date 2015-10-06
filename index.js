5301f65c2200bb4d1ed7d3109c84a757fd156a35
Josh Duff <me@JoshDuff.com>
2015-10-05 21:28:57 -0500

    Making the convertToHtml code simpler to read

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ var htmlify = require('./htmlify.js')
 var turnContentIntoAnArrayOfObjects = require('./noddity-template-transformer.js')
 
 module.exports = function (post, linkifier, options) {
-	var html = options && options.convertToHtml === false ? post.content : htmlify(post)
+	var convertToHtml = true
+
+	if (options && options.convertToHtml === false) {
+		convertToHtml = false
+	}
+
+	var html = convertToHtml ? htmlify(post) : post.content
 	html = linkifier.linkify(html)
 	return turnContentIntoAnArrayOfObjects(html)
 }
